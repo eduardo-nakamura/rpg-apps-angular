22174f9bb4382873b16b3a02fb2fd609ae26d1f2
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-10-16 02:21:54 -0300

    e

--- src/app/dice-roller/dice-roller.component.ts
+++ src/app/dice-roller/dice-roller.component.ts
@@ -6,23 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dice-roller.component.css']
 })
 export class DiceRollerComponent implements OnInit {
-  tipoDados: number
-  bonusDados = '';
-  qtdDados = '';
+  tipoDados = 'd4';
+  bonusDados = 0;
+  qtdDados = 1;
+  min = 1
+  max: number
   constructor() { }
 
   ngOnInit() {
   }
-  rolar(){
+  rolar() {
     //pegar infos
-    console.log(document.getElementById("qtdDados"))
-    
+    switch (this.tipoDados) {
+      case "d4":
+        this.max = 4;
+        break;
+      case "d6":
+        this.max = 6;
+        break;
+      case "d8":
+        this.max = 8;
+        break;
+      case "d10":
+        this.max = 10;
+        break;
+      case "d12":
+        this.max = 12;
+        break;
+      case "d20":
+        this.max = 20;
+        break;
+      case "d100":
+        this.max = 100;
+      break;
+    }
+    // gerar numeros
+    console.log(this.bonusDados + this.qtdDados)
+
   }
 
-  qtdDadosValue(event: KeyboardEvent) { // with type info
-    this.qtdDados = (event.target as HTMLInputElement).value ;    
+  qtdDadosValue(event: KeyboardEvent) {
+    this.qtdDados = (event.target as HTMLInputElement).valueAsNumber;
   }
-  bonusDadosValue(event: KeyboardEvent) { // with type info
-    this.bonusDados = (event.target as HTMLInputElement).value ;    
+  bonusDadosValue(event: KeyboardEvent) {
+    this.bonusDados = (event.target as HTMLInputElement).valueAsNumber;
+  }
+  selectChangeHandler(event: any) {
+    this.tipoDados = event.target.value;
   }
 }
