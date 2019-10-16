16117500d332b534089f8ee070615bbe9a292e21
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-10-16 15:55:40 -0300

    dice roller

--- src/app/dice-roller/dice-roller.component.ts
+++ src/app/dice-roller/dice-roller.component.ts
@@ -7,44 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiceRollerComponent implements OnInit {
   tipoDados = 'd4';
+  tipoDadoImg = '../../assets/d4.svg';
   bonusDados = 0;
   qtdDados = 1;
   min = 1
   max: number
+  rolagem: number
+  filtraNumero: any
+  somaDados: number
+  totalDados = []  
+  objHistoricoDados = []  
+
   constructor() { }
 
   ngOnInit() {
   }
   rolar() {
+    this.limparResultado()
     //pegar infos
     switch (this.tipoDados) {
       case "d4":
-        this.max = 4;
+        this.max = 4;        
         break;
       case "d6":
-        this.max = 6;
+        this.max = 6;        
         break;
       case "d8":
-        this.max = 8;
+        this.max = 8;        
         break;
       case "d10":
         this.max = 10;
         break;
       case "d12":
-        this.max = 12;
+        this.max = 12;      
         break;
       case "d20":
-        this.max = 20;
+        this.max = 20;        
         break;
       case "d100":
-        this.max = 100;
+        this.max = 100;        
       break;
     }
     // gerar numeros
-    console.log(this.bonusDados + this.qtdDados)
+    for (let i = 0; i < this.qtdDados; i++){
+      this.rolagem = (Math.floor((Math.random() * this.max) + this.min))  
+      if(this.tipoDados == 'd20' && this.rolagem == 20) {
+        alert('Acerto Crítico')
+      } else if (this.tipoDados == 'd20' && this.rolagem == 1){
+        alert('Erro Crítico')
+      }
+      if (this.totalDados.length == 0){
+        this.totalDados.push(this.rolagem)
+      } else {
+        this.totalDados.push('+',this.rolagem)
+      }  
+    }
+    if (this.bonusDados > 0){
+      this.totalDados.push('+',this.bonusDados)
+    }    
+    this.filtraNumero = this.totalDados.filter(a => Number(a))
+    this.somaDados = this.filtraNumero.reduce(function(a, b) {
+      return a + b;
+    });      
+    this.totalDados.push('=',this.somaDados)
 
+    this.objHistoricoDados.unshift(this.totalDados)
+    console.log(this.objHistoricoDados)
+    
+    
   }
-
   qtdDadosValue(event: KeyboardEvent) {
     this.qtdDados = (event.target as HTMLInputElement).valueAsNumber;
   }
@@ -53,5 +84,35 @@ export class DiceRollerComponent implements OnInit {
   }
   selectChangeHandler(event: any) {
     this.tipoDados = event.target.value;
+    switch (event.target.value){
+      case "d4":
+        this.tipoDadoImg = '../../assets/d4.svg';       
+        break;
+      case "d6":
+        this.tipoDadoImg = '../../assets/d6.svg';       
+        break;
+        case "d8":
+        this.tipoDadoImg = '../../assets/d8.svg';       
+        break;
+        case "d10":
+        this.tipoDadoImg = '../../assets/d10.svg';       
+        break;
+        case "d12":
+        this.tipoDadoImg = '../../assets/d12.svg';       
+        break;
+        case "d20":
+        this.tipoDadoImg = '../../assets/d20.svg';       
+        break;
+        case "d100":
+        this.tipoDadoImg = '../../assets/d100.svg';       
+        break;
+    }
+    // console.log(event.target.value)
+  }
+  limparResultado(){
+    this.totalDados = []
+  }
+  limparHistorico(){
+    this.objHistoricoDados = [] 
   }
 }
