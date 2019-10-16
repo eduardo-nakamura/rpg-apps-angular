01bd7f2bc99004c3b13a6626d1b6cde1c73c286e
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-10-16 00:39:23 -0300

    k

--- src/app/dice-roller/dice-roller.component.ts
+++ src/app/dice-roller/dice-roller.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dice-roller.component.css']
 })
 export class DiceRollerComponent implements OnInit {
-
+  tipoDados: number
+  bonusDados = '';
+  qtdDados = '';
   constructor() { }
 
   ngOnInit() {
   }
+  rolar(){
+    //pegar infos
+    console.log(document.getElementById("qtdDados"))
+    
+  }
 
+  qtdDadosValue(event: KeyboardEvent) { // with type info
+    this.qtdDados = (event.target as HTMLInputElement).value ;    
+  }
+  bonusDadosValue(event: KeyboardEvent) { // with type info
+    this.bonusDados = (event.target as HTMLInputElement).value ;    
+  }
 }
