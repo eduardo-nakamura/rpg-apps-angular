d57099867d2dc04c39b1f5a8d952f404543c050f
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-10-16 17:43:03 -0300

    escolherParrty

--- src/app/init-tracker/init-tracker.component.ts
+++ src/app/init-tracker/init-tracker.component.ts
@@ -6,11 +6,16 @@ import example from '../../assets/party.json';
   styleUrls: ['./init-tracker.component.css']
 })
 export class InitTrackerComponent implements OnInit {
-
+  partyOptions = example;
+  selectedParty = '';
+  partyMembers = '';
   constructor() { }
 
   ngOnInit() {
     console.log(example)
   }
-
+  selectChangeHandler(event: any) {    
+    this.selectedParty = event.target.value;
+    this.partyMembers = this.partyOptions[this.selectedParty]
+  }
 }
