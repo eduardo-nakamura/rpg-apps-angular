1928e1e7cf89010820f0cbb363f3faedf029d9d4
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-10-24 17:06:01 -0300

    tabela implemento

--- src/app/init-tracker/init-tracker.component.ts
+++ src/app/init-tracker/init-tracker.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import example from '../../assets/party.json';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface initTable {
+  name: string;
+  bonusInit: number;
+  diceInit: number;
+  totalInit: number;
+}
+const ELEMENT_DATA: initTable[] = []
 @Component({
   selector: 'app-init-tracker',
   templateUrl: './init-tracker.component.html',
@@ -8,7 +17,10 @@ import example from '../../assets/party.json';
 export class InitTrackerComponent implements OnInit {
   partyOptions = example;
   selectedParty = '';
-  partyMembers = '';
+  initObj = '';
+  displayedColumns: string[] = ['name', 'bonusInit', 'diceInit', 'totalInit'];    
+  dataSource = new MatTableDataSource<initTable>(ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit() {
@@ -16,7 +28,9 @@ export class InitTrackerComponent implements OnInit {
   }
   selectChangeHandler(event: any) {    
     this.selectedParty = event.target.value;
-    this.partyMembers = this.partyOptions[this.selectedParty]
-    console.log(this.partyMembers)
+    this.initObj = this.partyOptions[this.selectedParty].partyMember
+    this.dataSource = new MatTableDataSource<initTable>(example[this.selectedParty]);
+    console.log(this.dataSource)
   }
+  
 }
