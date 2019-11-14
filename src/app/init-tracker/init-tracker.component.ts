52f7be59013648f6a9856f68428207edb140d932
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-11-14 04:38:49 -0300

    sort

--- src/app/init-tracker/init-tracker.component.ts
+++ src/app/init-tracker/init-tracker.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import example from '../../assets/party.json';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface initTable {
   name: string;
@@ -15,55 +15,65 @@ const ELEMENT_DATA: initTable[] = []
   styleUrls: ['./init-tracker.component.css']
 })
 export class InitTrackerComponent implements OnInit {
-  ordenarChars:any;
+  proximo:number
+  ordenarChars: any;
   partyOptions = example;
   selectedParty = '';
   initObj = '';
   dadosResult: number
-  displayedColumns: string[] = ['name', 'bonusInit', 'diceInit', 'totalInit', 'delete'];    
+  displayedColumns: string[] = ['name', 'bonusInit', 'diceInit', 'totalInit', 'delete'];
   dataSource = new MatTableDataSource<initTable>(ELEMENT_DATA);
   addNewChar = ''
-  
+
   constructor() { }
 
-// Pesquisar Form Array
+  // Pesquisar Form Array
   ngOnInit() {
 
-    
-  }
-  selectChangeHandler(event: any) {    
-    this.selectedParty = event.target.value;    
-    this.initObj = example[this.selectedParty].partyMember
-    this.atualizarTabela()      
-  }
 
-  atualizarTabela(){
+  }
+  selectChangeHandler(event: any) {
+    this.selectedParty = event.target.value;
+    this.initObj = example[this.selectedParty].partyMember
+    this.atualizarTabela()
+  }
+  ordenarTabela() {
+    this.ordenarChars.sort(function (a, b) {
+      if (a.totalInit > b.totalInit) {
+        return -1;
+      }
+      if (a.totalInit < b.totalInit) {
+        return 1;
+      }
+      return 0;
+    });
+    this.atualizarTabela()
+    this.proximo = this.ordenarChars.length
+    document.querySelectorAll('ul li:nth-child(1)').style.backgroundImage = "url('newbg.png')"
+
+  }
+  atualizarTabela() {
     this.ordenarChars = example[this.selectedParty].partyMember
-    for( let i = 0; i < this.ordenarChars.length; i++){      
+    for (let i = 0; i < this.ordenarChars.length; i++) {
       this.ordenarChars[i].totalInit = this.ordenarChars[i].bonusInit + this.ordenarChars[i].diceInit
-    }        
-    // this.ordenarChars.sort(function (a, b) {
-    //   if (a.totalInit > b.totalInit) {
-    //     return -1;
-    //   }
-    //   if (a.totalInit < b.totalInit) {
-    //     return 1;
-    //   }      
-    //   return 0;
-    // });    
+    }
+
     this.dataSource = new MatTableDataSource<initTable>(this.ordenarChars);
   }
-  removeChar(i){
-    this.ordenarChars.splice(i,1)
+  removeChar(i) {
+    this.ordenarChars.splice(i, 1)
     console.log(i)
     this.atualizarTabela()
   }
-  addChar(){
-    let objNewChar = { name: '', bonusInit: 0, diceInit: 0, totalInit: 0,}
+  addChar() {
+    let objNewChar = { name: '', bonusInit: 0, diceInit: 0, totalInit: 0, }
     objNewChar.name = this.addNewChar
-    this.ordenarChars.push(objNewChar)    
-    this.atualizarTabela()  
+    this.ordenarChars.push(objNewChar)
+    this.atualizarTabela()
     this.addNewChar = ''
+  }
+  next(){
+    
   }
 
 }
