414cb536b513842d8e9450581de8e7202d829a83
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-01-31 16:10:25 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,30 @@ import { Router } from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('1s ease-out', 
+                    style({ height: 300, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({ height: 300, opacity: 1 }),
+            animate('1s ease-in', 
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent implements OnInit{
   menuContent = [
--- src/app/npc-generator/npc-generator.component.ts
+++ src/app/npc-generator/npc-generator.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as nameList from './names';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-npc-generator',
   templateUrl: './npc-generator.component.html',
@@ -15,7 +15,7 @@ export class NpcGeneratorComponent implements OnInit {
   selectedClass = "";
   levelNPC:number;
   generatedChar = {
-    name:"",race:"",class:"",gender:"",level:0,traits:[]
+    name:"",race:"",class:"",gender:"",level:0,traits:[],stats:{str: 0,dex:0,con:2,int:0,wis:0,cha:0}
   }
   generatedCharClipboard = ""
   gender;
@@ -30,15 +30,19 @@ export class NpcGeneratorComponent implements OnInit {
   }
   randomizeNPC(){
     if(this.selectedRace == ""){
-      let rollRace = Math.floor((Math.random() * this.races.length));
+      //rollDice
+      //let rollRace = Math.floor((Math.random() * this.races.length));
+      let rollRace = this.rollDice(this.races.length)
       this.selectedRace = this.races[rollRace];
     }
     if(this.selectedClass == ""){
-      let rollClass = Math.floor((Math.random() * this.classes.length));
+      //let rollClass = Math.floor((Math.random() * this.classes.length));
+      let rollClass = this.rollDice(this.classes.length)
       this.selectedClass = this.classes[rollClass];
     }    
     if(this.gender == undefined){      
-      let rollGender = Math.floor((Math.random() * 10));      
+      let rollGender = this.rollDice(10);      
+      //let rollGender = Math.floor((Math.random() * 10));     
       let genderSelect: number;
       if (rollGender <= 4){
         genderSelect = 0;
@@ -67,32 +71,32 @@ export class NpcGeneratorComponent implements OnInit {
     });    
     switch(this.gender){
       case "Masculino":        
-        rollName = Math.floor((Math.random() * npcRace[0].male.length) - 1);
+        rollName = this.rollDice(npcRace[0].male.length);
         nameNPC += npcRace[0].male[rollName];
         if (npcRace[0].lastName.length > 0){
-          let rollLastName = Math.floor((Math.random() * npcRace[0].lastName.length) - 1);        
+          let rollLastName = this.rollDice(npcRace[0].lastName.length);        
           nameNPC += " " + npcRace[0].lastName[rollLastName];
         }   
         this.generatedChar.name = nameNPC;
         break;
       case "Feminino":
-        rollName = Math.floor((Math.random() * npcRace[0].female.length) - 1);
+        rollName = this.rollDice(npcRace[0].female.length);
         nameNPC += npcRace[0].female[rollName];
         if (npcRace[0].lastName.length > 0){
-          let rollLastName = Math.floor((Math.random() * npcRace[0].lastName.length) - 1);        
+          let rollLastName = this.rollDice(npcRace[0].lastName.length);        
           nameNPC += " " + npcRace[0].lastName[rollLastName];
         }        
         this.generatedChar.name = nameNPC;
         break;
       case "Outro":
-        rollName = Math.floor((Math.random() * npcRace[0].male.length + npcRace[0].female.length) - 1);        
+        rollName = this.rollDice(npcRace[0].male.length + npcRace[0].female.length);        
         if(rollName < npcRace[0].male.length){
           nameNPC += npcRace[0].male[rollName];
         } else {
           nameNPC += npcRace[0].female[rollName - npcRace[0].male.length];
         }
         if (npcRace[0].lastName.length > 0){
-          let rollLastName = Math.floor((Math.random() * npcRace[0].lastName.length) - 1);        
+          let rollLastName = this.rollDice(npcRace[0].lastName.length);        
           nameNPC += " " + npcRace[0].lastName[rollLastName];
         }      
         this.generatedChar.name = nameNPC;
@@ -101,18 +105,25 @@ export class NpcGeneratorComponent implements OnInit {
 
   }
   generateTraits(){
-    let traitName = ["Aparência: ","Dons: ","Maneirismos: ","Vinculos: ","Defeitos/Segredos: "]
+    let traitNameDesc = ["Aparência: ","Dons: ","Maneirismos: ","Vinculos: ","Defeitos/Segredos: "]
     for (let i = 0; i < Object.keys(nameList.traits).length; i++){      
       let traitName = Object.keys(nameList.traits)[i];
-      let dice = this.rollDice(nameList.traits[traitName].length) 
-      console.log(traitName[0])     
-      if(nameList.traits[traitName][dice] == "Role duas vezes, ignorando resultados iguais a 10"){
-        let dice1 = this.rollDice(nameList.traits[traitName].length)
-        let dice2 = this.rollDice(nameList.traits[traitName].length)
-        this.generatedChar.traits.push(nameList.traits[traitName][dice1])
-        this.generatedChar.traits.push(nameList.traits[traitName][dice2])
+      let dice = this.rollDice(nameList.traits[traitName].length);
+      if(nameList.traits[traitName][dice] == "Role duas vezes, ignorando resultados iguais a 10"){            
+        let dice1 = this.rollDice(nameList.traits[traitName].length);
+        let dice2 = this.rollDice(nameList.traits[traitName].length);
+        do{
+          dice2 = this.rollDice(nameList.traits[traitName].length);
+        } while ( dice1 === dice2 )
+        if(dice1 == 9){
+          dice1 -= 1;
+        }
+        if(dice2 == 9){
+          dice2 -= 1;
+        }  
+        this.generatedChar.traits.push(traitNameDesc[i] + nameList.traits[traitName][dice1] + ", " + nameList.traits[traitName][dice2]);
       } else {
-        this.generatedChar.traits.push(traitName[i],nameList.traits[traitName][dice])
+        this.generatedChar.traits.push(traitNameDesc[i] + nameList.traits[traitName][dice]);
       }      
     }    
   }
@@ -123,16 +134,15 @@ export class NpcGeneratorComponent implements OnInit {
     this.gender = undefined;
     this.levelNPC = undefined;
     this.generatedChar = {      
-      name:"",race:"",class:"",gender:"",level:0,traits:[]
+      name:"",race:"",class:"",gender:"",level:0,traits:[],stats:{str: 0,dex:0,con:2,int:0,wis:0,cha:0}
     }
   }
 
   ngOnInit() {
-  //  this.generateName()
   
   }
-  rollDice(diceSides){
-    return Math.floor((Math.random() * diceSides));
+  rollDice(max){        
+    return Math.floor(Math.random() * (max - 0)) + 0;
   }
   copyText(val: string){
     let selBox = document.createElement('textarea');    
@@ -144,4 +154,3 @@ export class NpcGeneratorComponent implements OnInit {
     document.body.removeChild(selBox);
   }
 }
-// {"name":"Kilskan Imrad","race":"Draconato","class":"Paladino","gender":"Masculino","level":1}
