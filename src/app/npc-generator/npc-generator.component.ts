8f994e4333865db508cc3750c73f505b15794678
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-02-05 14:50:35 -0300

    level up

--- src/app/npc-generator/npc-generator.component.ts
+++ src/app/npc-generator/npc-generator.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as nameList from './objs';
+import { generateChar,formChar } from './objs';
+
+
 import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-npc-generator',
@@ -12,67 +15,53 @@ export class NpcGeneratorComponent implements OnInit {
   races = nameList.names.map(({ race }) => `${race}`)
   classes = nameList.classDnd.map(({ className }) => `${className}`);
   genderList = ["Masculino", "Feminino", "Outro"];
-  levelNPC: number;
-  selectedRace = "";
-  selectedClass = "";
-  gender;
-  generatedChar = {
-    name: "", 
-    race: "", 
-    class: "", 
-    gender: "", 
-    level: 0, 
-    traits: [], 
-    statsInit: { str: 0, con: 0, wis: 0, int: 0, cha: 0, dex: 0 },
-    statsCurrent: { str: 0, con: 0, wis: 0, int: 0, cha: 0, dex: 0 },
-    aligment:[],
-    pv:0
-  }
+  // levelNPC: number;
+  // selectedRace = "";
+  // selectedClass = "";
+  // gender;
+  generatedChar = new generateChar();
+  formChar = new formChar();
 
 
   constructor() { }
   generateNPC() {
     this.randomizeNPC();
     this.generateName();
-    this.generateStats();
-  
+    this.generateStats();  
   }
-
   randomizeNPC() {    
-    if (this.selectedRace == "") {
+    if (this.formChar.selectedRace == undefined) {
       let rollRace = this.rollDice(this.races.length,0);
-      this.selectedRace = this.races[rollRace];
+      this.formChar.selectedRace = this.races[rollRace];
     }
-    if (this.selectedClass == "") {
+    if (this.formChar.selectedClass == undefined) {
       let rollClass = this.rollDice(this.classes.length,0);
-      this.selectedClass = this.classes[rollClass];
+      this.formChar.selectedClass = this.classes[rollClass];
     }
-    if (this.gender == undefined) {
+    if (this.formChar.gender == undefined) {
       let rollGender = this.rollDice(10,0);      
       if (rollGender <= 4) {
-        this.gender = this.genderList[0];
+        this.formChar.gender = this.genderList[0];
       } else if (rollGender <= 8) {
-        this.gender = this.genderList[1];
+        this.formChar.gender = this.genderList[1];
       } else {
-        this.gender = this.genderList[2];
-      }      
+        this.formChar.gender = this.genderList[2];
+      }            
     }
-    if (this.levelNPC == undefined) {
-      this.levelNPC = 1;
-    }
-    this.generatedChar.race = this.selectedRace;
-    this.generatedChar.class = this.selectedClass;
-    this.generatedChar.gender = this.gender;
-    this.generatedChar.level = this.levelNPC;  
+
+    this.generatedChar.race = this.formChar.selectedRace;
+    this.generatedChar.class = this.formChar.selectedClass;
+    this.generatedChar.gender = this.formChar.gender;
+    this.generatedChar.level = this.formChar.levelNPC;  
   }
 
   generateName() {
     if (this.generatedChar.name == ""){
-      let raceName = this.selectedRace;
+      let raceName = this.formChar.selectedRace;
       let npcRace = nameList.names.filter(function (hero) {
         return hero.race == raceName;
       });
-      switch (this.gender) {
+      switch (this.formChar.gender) {
         case "Masculino":
           this.generatedChar.name += npcRace[0].male[this.rollDice(npcRace[0].male.length,0)];
           if (npcRace[0].lastName.length > 0) {            
@@ -101,20 +90,23 @@ export class NpcGeneratorComponent implements OnInit {
   }
 
   generateStats() {
-   let raceFilter = this.selectedRace
-   let classFilter = this.selectedClass
-   this.generatedChar.level = this.levelNPC
+   let raceFilter = this.formChar.selectedRace   
+   let classFilter = this.formChar.selectedClass
+   this.generatedChar.level = this.formChar.levelNPC
+   let raceStats = nameList.races.filter(function (race) {      
+    race.race == raceFilter    
+    return race.race == raceFilter;
+   });    
    if(this.generatedChar.statsInit.str == 0){
     let rollStats = new Array(6).fill(null)
     for (let i = 0; i < 6; i++) {
       let rollDiceStats = new Array(4).fill(null)
-      rollDiceStats = rollDiceStats.map(() => (this.rollDice(6,1)));
+      rollDiceStats = rollDiceStats.map(() => (this.rollDice(7,1)));      
       rollDiceStats.sort();
       rollDiceStats.shift();
       rollStats[i] = rollDiceStats.reduce(function (previous, current) {
         return previous + current;
-      }, 0);
-      console.log(rollDiceStats,rollStats[i])
+      }, 0);      
       switch(classFilter){
         case"Bárbaro":this.generatedChar.statsInit={cha:rollStats[0],int:rollStats[1],wis:rollStats[2],dex:rollStats[3],con:rollStats[4],str:rollStats[5]};break;
         case"Bardo":this.generatedChar.statsInit={str:rollStats[0],con:rollStats[1],wis:rollStats[2],int:rollStats[3],cha:rollStats[4],dex:rollStats[5]};break;
@@ -127,25 +119,34 @@ export class NpcGeneratorComponent implements OnInit {
         case"Monge":this.generatedChar.statsInit={int:rollStats[0],wis:rollStats[1],cha:rollStats[2],con:rollStats[3],str:rollStats[4],dex:rollStats[5]};break;
         case"Paladino":this.generatedChar.statsInit={int:rollStats[0],dex:rollStats[1],con:rollStats[2],cha:rollStats[3],wis:rollStats[4],str:rollStats[5]};break;
         case"Patrulheiro":this.generatedChar.statsInit={int:rollStats[0],wis:rollStats[1],con:rollStats[2],cha:rollStats[3],str:rollStats[4],dex:rollStats[5]};break;
-      }
+      }      
+    }    
+    for (let property in raceStats[0].stats){
+      if(raceStats[0].stats[property]){        
+        this.generatedChar.statsInit[property] += raceStats[0].stats[property]
+      }      
     }
-    // rollStats.sort(this.sortNumAscend) 
-    console.log(this.generatedChar.statsInit)
-    
-   }
+    this.generatedChar.statsCurrent = this.generatedChar.statsInit    
+    if(this.generatedChar.level > 1){
+      this.levelUp()
+    }
+   }  
   }
-
+  levelUp(){
+ 
+  }
   sortNumAscend(a, b) {
     return a > b ? 1 : b > a ? -1 : 0;
   }
   resetNPC() {
-   
+   this.generatedChar = new generateChar()
+   this.formChar = new formChar();
   }
   alignmentNPC(){
    
   }
   ngOnInit() {
-    console.log(this.races.length)
+    console.log(this.formChar)
   }
   rollDice(max, min) {
     return Math.floor(Math.random() * (max - min)) + min;
