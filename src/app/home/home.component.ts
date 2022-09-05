a0b21c0db0ed5953919fd6e66ebee4527e0302ac
eduardo-nakamura <edu-nakamura@hotmail.com>
2022-09-05 11:32:23 -0300

    alt images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,33 +10,33 @@ export class HomeComponent implements OnInit {
 
   cards = [
     {
-      // imagem:'./assets/dice.png',
-      imagem:'https://via.placeholder.com/300x300',      
+      imagem:'./assets/dice.png',
+      // imagem:'https://via.placeholder.com/300x300',      
       titulo:'Dice Roller',
       descricao:'Aplicativo para Rolar Dados',
       path:'dice-roller'
     },
     {
-      // imagem:'./assets/init-tracker.png',
-      imagem:'https://via.placeholder.com/300x300',      
+      imagem:'./assets/init-tracker.png',
+      // imagem:'https://via.placeholder.com/300x300',      
       titulo:'Initiative Tracker',
       descricao:'Aplicativo para determinar iniciativa',
       path:'init-tracker'
     },
     {
-      imagem:'https://via.placeholder.com/300x300',
+      imagem:'./assets/hook.png',
       titulo:'Game Hooks',
       descricao:'Alguns ganchos de história para iniciar uma aventura',
       path:'game-hook'
     },
     {
-      imagem:'https://via.placeholder.com/300x300',
+      imagem:'./assets/loot.png',
       titulo:'Loot Generator',
       descricao:'Determine os tesouros do seus jogadores aqui',
       path:'loot-generator'
     },
       {
-      imagem:'https://via.placeholder.com/300x300',
+      imagem:'./assets/npc-gen.png',
       titulo:'NPC Generator',
       descricao:'Gere personagens para sua campanha',
       path:'npc-generator'
